0e85dc0163ac4417d50f55d545c3d06c23605c70
wkslade <wkslade@gmail.com>
2026-01-10 21:02:44 -0700

    I Wrot a program that will ask the user for their % grade they got in a class. It will then determin what letter grade they recived and wether or not they passed the class.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter in your grade in the class");
+        string userInputGrade = Console.ReadLine();
+        int userGrade = int.Parse(userInputGrade);
+        string letterGrade = "A"; 
+        if (userGrade >= 90) {
+            letterGrade = "A";
+        }
+        else if (userGrade >= 80 && userGrade < 90 ) {
+            letterGrade = "B";
+        }
+        else if (userGrade < 80 && userGrade >= 70) {
+            letterGrade = "C";
+        }
+        else if (userGrade < 70 && userGrade >= 60) {
+            letterGrade = "D";
+        }
+        else {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade in the class in the class is {letterGrade}"  );
+        if (userGrade >= 70){
+            Console.WriteLine("Congradualations! you passed your class. Well done :)");
+        }
+        else {
+            Console.WriteLine("Better luck next time.");
+        }
     }
 }
