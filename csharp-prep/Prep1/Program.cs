060e8ab220a4faa988cdef79311b41645ffd74a3
wkslade <wkslade@gmail.com>
2026-01-10 21:09:54 -0700

    I wrote a program that will ask the user for their fist and last name. It then introduce them like James bon. Ex if their name was Dallas Walker it would say. Your name is Walker, Dallas Walker.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+            // Console.WriteLine("Hello World!");
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string Last_name = Console.ReadLine();
+        Console.WriteLine("Your name is " + Last_name + ", " + first_name + " " + Last_name + ".");
+
     }
 }
