8cb6d74ee910ede2d756b558d0bcd63fc3f91e3e
wkslade <wkslade@gmail.com>
2026-01-14 20:37:30 -0700

    I wrote a program that will generat a random number from 1-100, it then asks the user to gess the number. if they gess too high or low the program will tell them wether or not it is too high or low and give them an other guess. once the person guesses the correct number it will congradualate them and display how many times it took them to guess the correct number. Finaly it will ask the user if they would want to play again if the user says yes then it will generat a new number, if the user says no the program will end.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string keepGoing = "yes";
+        while (keepGoing == "yes"){
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        //int magicNumber;
+        //Console.Write("What is the magic number?");
+        //magicNumber = int.Parse(Console.ReadLine());
+                int guess;
+                int count = 1;
+        Console.Write("What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+        while (magicNumber != guess) {
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        Console.Write("What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+        count ++ ; 
+        }
+        Console.WriteLine("You guessed it!");
+        Console.WriteLine($"it took {count} tries :)");
+        Console.Write("Would you like to play again? ");
+        keepGoing = Console.ReadLine();
+        count = 1;
+        }
     }
 }
