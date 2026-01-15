63f26a37fec57ef22fd35c530a08100930d39e83
wkslade <wkslade@gmail.com>
2026-01-14 21:43:21 -0700

    I wrote a program that will make a list of numbers by asking the user to continue inputing numbers until they input 0. then it will sum all the numbers in the list, calculate the average number and it will figure out which number was the biggest and display the results to the user.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>(); // sets up an empty list
+        Console.Write("Give me a number for the list: "); // ask the user for a number
+        int number = int.Parse(Console.ReadLine()); // saves that number in a variable
+        while (number != 0) // sets up a loop to keep asking for numbers until they put in 0
+        {
+            numbers.Add(number); // adds the last number the user inputer on the end of the list
+            Console.Write("Give me an aditional number for the list: "); // asks again for a number
+            number = int.Parse(Console.ReadLine());// saves the using to the variable number
+        }
+        numbers.Add(number);// adds the last number the user inputed
+        int total = 0; // sets up a variable total
+        foreach (int numberes in numbers) // a for loop that cycles through the list numbers and adds them all together to get a total
+        {
+            //total += numbers[numberes];
+            Console.WriteLine(numberes);
+            total += numberes;// adds the number in the list to the total
+        }
+        int biggestNumber = 0; // sets up a variable for the biggest number
+        foreach (int numberes in numbers)// sets up a for loop that will check to see if a number in the list is bigger than the biggest number see so far starting at zero. in the end the larges number will be strored in the variable
+        {
+           if (numberes > biggestNumber) // checks to see if the current number is bigger than the bigges
+            {
+                biggestNumber = numberes;// If yes, it will replace the biggest number. 
+            }
+        }
+        // the next few lines will display the results. 
+        Console.WriteLine($"The sum is: {total}");
+        float average = total/numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The biggest number is: {biggestNumber}");
+
     }
 }
