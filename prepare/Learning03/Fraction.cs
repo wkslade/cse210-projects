e4394d490544485aede7fb393a2009137604a4e8
wkslade <wkslade@gmail.com>
2026-02-04 22:23:59 -0700

    I wrote a fraction class that has two privat varables for the top and bottom of the fraction. I then wrote some Set and Get funtions that allow the user to set and use the fraction class but it still leave the fraction class in control of its constructors.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+    public void SetFraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+    public void SetTop(int userTop)
+    {
+        _top = userTop;
+    }
+    public void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+    public void SetTopAndBottom(int userTop,int userBottom)
+    {
+        _top = userTop;
+        _bottom = userBottom;
+    }
+    public void DisplayFraction(){
+        Console.WriteLine($"{_top}/{_bottom}");
+    }
+    public int GetTop(){
+        return _top;
+    }
+    public int GetBottom(){
+        return _bottom;
+    }
+    public string GetFractionString(){
+        return $"{_top}/{_bottom}";
+    }
+    public double GetDecimalValue(){
+        double dec = (double)_top / _bottom;
+        return dec;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction test = new Fraction();
+        test.SetFraction();
+        test.DisplayFraction();
+        // test.SetTop(5);
+        // test.DisplayFraction();
+        // test.SetBottom(8);
+        // test.DisplayFraction();
+        // test.SetTopAndBottom(6,7);
+        // test.DisplayFraction();
+        // int top = test.GetTop();
+        // int bottom = test.GetBottom();
+        // Console.WriteLine($"the top is {top}, and the bottom is {bottom}:");
+        // string fraction = test.GetFractionString();
+        // Console.WriteLine(fraction);
+        // double decimole = test.GetDecimalValue();
+        // Console.WriteLine($"{decimole}");
+        Random num = new Random();
+        for (int i = 0; i <= 19; i++){
+            int top = num.Next();
+            test.SetTop(top);
+            int bottom = num.Next();
+            test.SetBottom(bottom);
+            //int getTop = test.GetTop();
+            //int getBottom = test.GetBottom();
+            string frac = test.GetFractionString();
+            double dec = test.GetDecimalValue();
+            Console.WriteLine($"Fraction 1: string: {frac} Number: {dec}");
+        }
     }
 }
